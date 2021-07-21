bc5754a7430735747944b49820d42253ec41ff77
= <22709469@student.uwa.edu.au>
2021-07-21 19:00:39 +0800

    SEO Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,4 +12,16 @@ const Index = () => {
   );
 };
 
+// istanbul ignore next
+export async function getServerSideProps() {
+  return {
+    props: {
+      keywords:
+        'csmarks, uwa csmarks, csse csmarks, University of Western Australia, csse, uwa, uws cs, computer science, cits, csse marks',
+      description:
+        "University of Western Australia, csse csmarks page redirect button. Since the homepage doesn't exists, this what we all use instead.",
+    },
+  };
+}
+
 export default Index;
