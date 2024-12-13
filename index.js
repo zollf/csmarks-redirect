797bf0d0cffc2033adb8f74921f03301527b4d52
zollf <dylankennedy2000@hotmail.com>
2024-12-13 20:03:53 +0800

    Change hosting to AWS, added auto-redirect, simplify setup

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function redirect() {
   const date = new Date();
   date.setTime(date.getTime() + (365*24*60*60*1000));
   document.cookie = `auto_redirect=true; expires=${date.toUTCString()}; path=/`;
-  document.getElementById("csmarks-link").click();
+  window.location.assign("https://secure.csse.uwa.edu.au/run/csmarks");
 }
